ccdbe32a7f522d7af152b37ff30f1a578aa33c2f
Gama Suárez <gamalielsuarez5@gmail.com>
2018-11-23 10:09:17 -0600

    Le dimos efecto de paralax a el header

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 'use strict';
 // Dom Elements
-const header = document.querySelector('header');
-const red = document.querySelector('#red');
+const header = document.querySelector('.header');
+const titulo = document.querySelector('.titulo');
 
 // Event Listeners - Este escucha el evento de scroll
 document.addEventListener('scroll', scrollController)
@@ -12,35 +12,17 @@ function scrollController() {
   // Constante que guarda el valor del scroll que ha hecho la ventana
   const windowScroll = window.pageYOffset;
   headerHeightChange(windowScroll);
-  headerColorChange(windowScroll);
-  redAnimation(windowScroll);
 }
 
 // headerHeightChange cambia el alto del header y su opacidad
 function headerHeightChange(windowScroll) {
-  if(windowScroll > 0) {
+  if(windowScroll > 170) {
     header.classList.add('scroll-up');
+    titulo.classList.add('cambioTitulo')
   } else {
     header.classList.remove('scroll-up');
-  }
-}
+    titulo.classList.remove('cambioTitulo');
 
-// headerColorChange cambia el colopr del header al llegar al cuadro de color
-function headerColorChange(windowScroll) {
-  const redScrollValue = red.offsetTop;
-  if(windowScroll >= redScrollValue - 45) {
-    header.classList.add('scroll-up-red');
-  } else {
-    header.classList.remove('scroll-up-red');
-  }
-}
 
-// redAnimation hace que el cuadro de vueltas
-function redAnimation(windowScroll) {
-  const redScrollValue = red.offsetTop;
-  if(windowScroll >= redScrollValue - 300) {
-    red.style.transform = "rotate(" + windowScroll + "deg)";
-  } else{
-    red.classList.remove('animated');
   }
 }
